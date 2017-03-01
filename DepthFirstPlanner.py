aa3527f812d11c115077bc5b28e53360185f84fd
Brad Factor <bfactor@andrew.cmu.edu>
2017-03-01 18:06:24 -0500

    Wrote first implementation of DFS. Not expected to work initially, but will debug once supporting functions are available.

--- DepthFirstPlanner.py
+++ DepthFirstPlanner.py
@@ -6,15 +6,34 @@ class DepthFirstPlanner(object):
         self.nodes = dict()
 
     def Plan(self, start_config, goal_config):
-        
-        plan = []
-        
+
         # TODO: Here you will implement the depth first planner
         #  The return path should be a numpy array
         #  of dimension k x n where k is the number of waypoints
         #  and n is the dimension of the robots configuration space
 
-        plan.append(start_config)
-        plan.append(goal_config)
+        #plan.append(start_config)
+        #plan.append(goal_config)
+        
+        plan = []
+	visited = []
 
-        return plan
+	plan_config = []
+
+	start_node = self.planning_env.discrete_env.ConfigurationToNodeId(start_config)
+	goal_node = self.planning_env.discrete_env.ConfigurationToNodeId(goal_config)
+
+	plan.append(start_node)
+	while plan is not empty:
+		node = plan.pop()
+		if node == goal_node:
+			for node in plan:
+				plan_config.append(self.planning_env.discrete_env.NodeIdToConfiguration(node))
+				return plan_config
+		if node is not in visited:
+			visited.append(node)
+			successors = self.planning_env.GetSuccessors(node)
+			for all neighbors in successors:
+				plan.append(neighbors)
+
+        return None
